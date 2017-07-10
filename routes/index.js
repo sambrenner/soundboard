71c42e3ea18610e9f91e1cfc125974ed5abe5a05
sambrenner <samjbrenner@gmail.com>
2017-07-09 22:23:56 -0400

    removing original version files

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-
-exports.index = function(req, res){
-  var sounds = fs.readdirSync('./public/sounds');
-
-  res.render('index', { title: 'Soundboard', sounds: sounds });
-};
