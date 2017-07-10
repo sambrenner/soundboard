41bb729ae8e1e3d154e72459f6285826c115f77e
sambrenner <samjbrenner@gmail.com>
2017-07-10 09:45:12 -0400

    bodyparser before session

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const api = require("./routes/api/v1");
 
 app.keys = [process.env.SECRET_KEY];
 
-app.use(bodyParser());
 app.use(session({}, app));
+app.use(bodyParser());
 app.use(passport.initialize());
 app.use(passport.session());
 
