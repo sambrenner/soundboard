2cdb4e4060b9c7414621eb198662a4d740f699fe
sambrenner <samjbrenner@gmail.com>
2017-07-12 09:47:21 -0400

    views

--- index.js
+++ index.js
@@ -2,15 +2,26 @@ require("dotenv").config();
 
 const koa = require("koa");
 const bodyParser = require("koa-bodyparser");
+const render = require("koa-ejs");
 const session = require("koa-session");
 
-const app = new koa();
-
 const db = require("./db");
+const path = require("path");
 
 const passport = require("./util/passport");
 const auth = require("./routes/auth")(passport);
 const api = require("./routes/api/v1");
+const index = require("./routes");
+
+const app = new koa();
+
+render(app, {
+    root: path.join(__dirname, "views"),
+    layout: "template",
+    viewExt: "ejs",
+    cache: process.env.NODE_ENV != "development",
+    debug: false
+});
 
 app.keys = [process.env.SECRET_KEY];
 
